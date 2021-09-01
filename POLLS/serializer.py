376de0390dd0cfa670c5d9d8ae2eb654cf0c97b9
Hackersmate-Aditya <68347060+Hackersmate-Aditya@users.noreply.github.com>
2021-09-01 10:11:52 +0530

    Update serializer.py

--- POLLS/serializer.py
+++ POLLS/serializer.py
@@ -11,7 +11,7 @@ class ChoiceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Choice
-        fields = '__all__'
+        fields = '__all__'                                       #selecting all fields
 
 
 
@@ -33,13 +33,13 @@ class QuestionSerializer(serializers.ModelSerializer):
        def validate(self, attrs):
         errors = list()
 
-        if attrs['pub_date'] > datetime.datetime.now(attrs['pub_date'].tzinfo):
+        if attrs['pub_date'] > datetime.datetime.now(attrs['pub_date'].tzinfo):                       #comparing our pub_date with current one and if pub_date>current,, it will throw an error
             errors.append('pub_date cannot be in future')
 
-        if attrs['question_text'].find('?')== -1:
+        if attrs['question_text'].find('?')== -1:                                              #if question_text doesnt have '?', error will be appended
             errors.append('question_text must contain question mark....')
 
         if attrs.__len__() > 0:
-            raise serializers.ValidationError(' _###_ '.join(x for x in errors))
+            raise serializers.ValidationError(' _###_ '.join(x for x in errors))                       
 
         return attrs
